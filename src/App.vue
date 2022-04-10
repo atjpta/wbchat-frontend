e7bb411c3e1803b754fb4c3d2b86a568cc940f1c
atjpa <caoducan351@gmail.com>
2022-04-10 21:07:38 +0700

    cấu hình frontend cho dự án

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-blue-800">
+    <p >hello vuejs!!!</p>
+  </div>
+</template>
+
